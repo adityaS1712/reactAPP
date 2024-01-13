45ca215f1e153186ca83230dc1d18bce5b53a22a
adityaS1712 <89769748+adityaS1712@users.noreply.github.com>
2024-01-13 13:11:28 +0530

    useMemo used to prevent rerender

--- reactApp/src/App.jsx
+++ reactApp/src/App.jsx
@@ -1,48 +1,38 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useEffect } from "react";
 import axios from "axios"
 
 function App() {
-  // state? useState
-  const [selectedId, setSelectedId] = useState(1);
+  const [counter, setCounter] = useState(0);
+  const [inputValue,setInputValue]=useState(1);
+
+  let count = useMemo(()=>{
+  let finalCount =0;
+  console.log("memo is called everytime");
+  for(let i=1;i<=inputValue;i++){
+    finalCount=finalCount+i;
+  }
+  return finalCount
+
+ },[inputValue] );
 
   return <div>
-    <button onClick={() => setSelectedId(1)}>1</button>
-    
-    <button onClick={function() {
-      setSelectedId(2);
-    }}>2</button>
-    
-    <button onClick={function() {
-      setSelectedId(3);
-    }}>3</button>
-    
-    <Todo id={selectedId} />
+    <input onChange={function (e){
+        setInputValue(e.target.value);
+    }} placeholder={"Find sum from 1 to n"}></input>
+    <br/>
+  Sum from 1 to {inputValue} is {count}
+    <br/>
+
+    <button onClick={()=>{
+      setCounter(counter+1);
+    }}> Counter ({counter})</button>
   </div>
+
+
+  
 }
 
-function Todo({id}) {
-  const [todo, setTodo] = useState({});
 
-  // implement effect here
-  useEffect(() => {
-    setTimeout(() => {
-      axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
-        .then(response => {
-          setTodo(response.data.todo)
-        })
-    } , 5000)
-  }, [id])
-
-  return <div>
-    Id: {id}
-    <h1>
-      {todo.title}
-    </h1>
-    <h4>
-      {todo.description}
-    </h4>
-  </div>
-}
 
 export default App;
